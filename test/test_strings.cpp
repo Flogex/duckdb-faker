2525b1f5fe1522d36633b403f923c1d9c402ea9d
Florian <gerlinghoff@startmail.com>
2025-11-01 15:58:57 +0100

    Add more coverage for 'random_string'
    ... and move slow tests to separate TEST_CASE

--- test/test_strings.cpp
+++ test/test_strings.cpp
@@ -6,6 +6,7 @@
 
 #include <cctype>
 #include <cstdint>
+#include <limits>
 #include <map>
 #include <string>
 
@@ -63,6 +64,12 @@ TEST_CASE_METHOD(DatabaseFixture, "random_string length", "[strings]") {
     }
 
     SECTION("Should reject invalid length arguments") {
+        const auto res = con.Query("FROM random_string(length='invalid')");
+        REQUIRE(res->HasError());
+        CHECK_THAT(res->GetError(), ContainsSubstring("Could not convert string 'invalid' to UINT64"));
+    }
+
+    SECTION("Should reject length arguments that are out of range") {
         const auto invalid_length = GENERATE("-1", "18446744073709551616" /* UINT64_MAX + 1 */);
 
         const auto query = std::format("FROM random_string(length={})", invalid_length);
@@ -72,7 +79,7 @@ TEST_CASE_METHOD(DatabaseFixture, "random_string length", "[strings]") {
         CHECK_THAT(res->GetError(), ContainsSubstring("value is out of range"));
     }
 
-    SECTION("Should throw error when length and min/max are both specified") {
+    SECTION("Should throw error when length and min/max length are both specified") {
         const auto query = GENERATE("FROM random_string(length=10, min_length=1)",
                                     "FROM random_string(length=10, max_length=5)",
                                     "FROM random_string(length=10, min_length=1, max_length=5)");
@@ -96,15 +103,6 @@ TEST_CASE_METHOD(DatabaseFixture, "random_string length", "[strings]") {
         }
     }
 
-    /* This test is too slow
-    SECTION("Should be able to produce strings with minimum_length UINT64_MAX") {
-        const auto query = std::format("FROM random_string(min_length={}) LIMIT 1", UINT64_MAX);
-        const auto res = con.Query(query);
-        const auto val = res->GetValue(0, 0).GetValue<std::string>();
-        REQUIRE(val.size() == UINT64_MAX);
-    }
-    */
-
     SECTION("Should produce strings with given maximum length") {
         const uint64_t max_length = GENERATE(10, 1000);
 
@@ -119,8 +117,9 @@ TEST_CASE_METHOD(DatabaseFixture, "random_string length", "[strings]") {
     }
 
     SECTION("Should produce strings with given minimum and maximum length") {
-        const uint64_t min_length = GENERATE(10, 100);
-        const uint64_t max_length = min_length * 2;
+        auto [min_length, max_length] = GENERATE(std::make_tuple<uint64_t, uint64_t>(10, 20),
+                                                 std::make_tuple<uint64_t, uint64_t>(100, 200),
+                                                 std::make_tuple<uint64_t, uint64_t>(10, 10));
 
         const auto query =
             std::format("FROM random_string(min_length={}, max_length={}) LIMIT {}", min_length, max_length, LIMIT);
@@ -134,11 +133,33 @@ TEST_CASE_METHOD(DatabaseFixture, "random_string length", "[strings]") {
         }
     }
 
-    SECTION("Should handle min_length greater than max_length") {
+    SECTION("Should throw error if min_length is greater than max_length") {
         const auto query = "FROM random_string(min_length=100, max_length=50)";
         const auto res = con.Query(query);
         REQUIRE(res->HasError());
         CHECK_THAT(res->GetError(), ContainsSubstring("min_length cannot be greater than max_length"));
+    }
+
+    SECTION("Should choose sane default for max_length if only min_length specified") {
+        // For min_length < 10, max_length is 20.
+        // For min_length >= 10, max_length should be min_length * 2.
+        auto [min_length, max_length] = GENERATE(std::make_tuple<uint64_t, uint64_t>(1, 20),
+                                                 std::make_tuple<uint64_t, uint64_t>(9, 20),
+                                                 std::make_tuple<uint64_t, uint64_t>(10, 20),
+                                                 std::make_tuple<uint64_t, uint64_t>(50, 100),
+                                                 std::make_tuple<uint64_t, uint64_t>(100, 200),
+                                                 std::make_tuple<uint64_t, uint64_t>(1000, 2000));
+
+        const auto query =
+            std::format("FROM random_string(min_length={}, max_length={}) LIMIT {}", min_length, max_length, LIMIT);
+        const auto res = con.Query(query);
+
+        sanity_check(res);
+        for (uint32_t row = 0; row < LIMIT; row++) {
+            const auto val = res->GetValue(0, row).GetValue<std::string>();
+            CHECK(val.size() >= min_length);
+            CHECK(val.size() <= max_length);
+        }
     }
 
     SECTION("Should produce strings of different lengths") {
@@ -162,33 +183,54 @@ TEST_CASE_METHOD(DatabaseFixture, "random_string length", "[strings]") {
     }
 }
 
+TEST_CASE_METHOD(DatabaseFixture, "random_string length slow", "[strings][.slow]") {
+    SECTION("Should be able to produce strings with minimum_length UINT64_MAX") {
+        const auto query = std::format("FROM random_string(min_length={}) LIMIT 1", UINT64_MAX);
+        const auto res = con.Query(query);
+        const auto val = res->GetValue(0, 0).GetValue<std::string>();
+        REQUIRE(val.size() == UINT64_MAX);
+    }
+
+    SECTION("Should not overflow when calculating max_length for large min_length") {
+        // max_length should not overflow
+        auto [min_length, max_length] =
+            GENERATE(std::make_tuple<uint64_t, uint64_t>(UINT64_MAX / 2, UINT64_MAX),
+                     std::make_tuple<uint64_t, uint64_t>(UINT64_MAX / 2 + 5000, UINT64_MAX));
+
+        const auto query =
+            std::format("FROM random_string(min_length={}, max_length={}) LIMIT 3", min_length, max_length);
+        const auto res = con.Query(query);
+
+        for (uint32_t row = 0; row < res->RowCount(); row++) {
+            const auto val = res->GetValue(0, row).GetValue<std::string>();
+            CHECK(val.size() >= min_length);
+            CHECK(val.size() <= max_length);
+        }
+    }
+}
+
 TEST_CASE_METHOD(DatabaseFixture, "random_string casing", "[strings]") {
-    SECTION("Should produce lowercase strings by default") {
-        const auto query = std::format("FROM random_string() LIMIT {}", LIMIT);
+    auto test_lower_casing = [&](const std::string& casing_param = "") {
+        const auto casing_clause = casing_param.empty() ? "" : std::format("casing='{}'", casing_param);
+        const auto query = std::format("FROM random_string({}) LIMIT {}", casing_clause, LIMIT);
         const auto res = con.Query(query);
 
         sanity_check(res);
         for (uint32_t row = 0; row < LIMIT; row++) {
             const auto val = res->GetValue(0, row).GetValue<std::string>();
             CAPTURE(val);
-            for (char c : val) {
+            for (const char c : val) {
                 REQUIRE(std::islower(c));
             }
         }
+    };
+
+    SECTION("Should produce lowercase strings by default") {
+        test_lower_casing();
     }
 
     SECTION("Should produce lowercase strings when specified") {
-        const auto query = std::format("FROM random_string(casing='lower') LIMIT {}", LIMIT);
-        const auto res = con.Query(query);
-
-        sanity_check(res);
-        for (uint32_t row = 0; row < LIMIT; row++) {
-            const auto val = res->GetValue(0, row).GetValue<std::string>();
-            CAPTURE(val);
-            for (char c : val) {
-                REQUIRE(std::islower(c));
-            }
-        }
+        test_lower_casing("lower");
     }
 
     SECTION("Should produce uppercase strings when specified") {
